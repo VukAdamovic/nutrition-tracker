f6503e0f1b1419b6db7af20eaa26a770c4d80973
Mitar <mkajganic9921rn@raf.rs>
2023-06-17 12:19:38 +0200

    Izmene za Timber, i dalje ne radi

--- app/src/main/java/com/example/myapplication/application/MyApplication.java
+++ app/src/main/java/com/example/myapplication/application/MyApplication.java
@@ -9,6 +9,9 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+//        if (BuildConfig.DEBUG) {
+//            Timber.plant(new Timber.DebugTree());
+//        }
         initDagger();
     }
 
